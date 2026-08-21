1fc8c803e55c712758e8ed4d13645b790f08fa29
clementbedour <bedour@insa-toulouse.fr>
2026-08-21 15:10:08 +0200

    volpick + pyocto

--- prg_bash/scp.sh
+++ prg_bash/scp.sh
@@ -13,25 +13,28 @@ script -q -c "sshpass -p '$MDP' scp ../prg_python/IA_seisbench_Tuning.py clov@nu
 script -q -c "sshpass -p '$MDP' scp ../prg_python/detection_nouv.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 script -q -c "sshpass -p '$MDP' scp ../prg_python/extraire_nouv.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 script -q -c "sshpass -p '$MDP' scp ../prg_python/fusion_dataset.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/association_pyocto.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 
 #fichier image IA classique
 script -q -c "sshpass -p '$MDP' scp ../prg_python/test_IA.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 script -q -c "sshpass -p '$MDP' scp ../prg_python/image_event_day.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-script -q -c "sshpass -p '$MDP' scp ../prg_python/metrics.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/loss_curve.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 script -q -c "sshpass -p '$MDP' scp ../prg_python/compare_trust.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/compare_noise.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+
 
 #fichier pour IA base VCSEIS
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_prepare_format.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_prepare_noise.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_fusion_dataset.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/IA_volpick_Tuning.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_detection_nouv.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_extraire_nouv.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_prepare_format.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_prepare_noise.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_fusion_dataset.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/IA_volpick_Tuning.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_detection_nouv.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_extraire_nouv.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 
 #fichier image IA volpick
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_test_IA.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_image_event_day.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_metrics.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
-#script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_compare_trust.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_test_IA.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_image_event_day.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_metrics.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
+script -q -c "sshpass -p '$MDP' scp ../prg_python/volpick_compare_trust.py clov@nuwa.aero.obs-mip.fr:/home/clov/seisbench_VT/prg_python" /dev/null
 
 echo "FIN"
